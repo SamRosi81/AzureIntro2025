8b93a508d78331b4f37bc055cff17afb64380e93
Sam Roshan <Sam.Roshan@bergpropulsion.com>
2025-11-16 20:44:03 +0100

    fix rg name

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "rg-kolad-sch"
+  default     = "rg-sroshan-sch"
 }
 
 variable "subscription_id" {
